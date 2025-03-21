ed249f65fe2a9388ec644a7cb37438b57460c265
Wesley Melo <wesleymelo@MacBook-Air-de-Wesley.local>
2025-03-21 19:21:06 -0300

    update navegacao coordinforma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import CoordInformaPage from './pages/CoordInformaPage';
 import RespInformaPage from './pages/RespInformaPage';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/coordinforma" element={<CoordInformaPage />} />
         <Route path="/respinforma" element={<RespInformaPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
